3c1a498f805e6611f643ea26be16542a948913fc
bmcferron <bmcferron@graphicvillage.com>
2024-02-18 02:19:27 -0500

    Changed architecture to allow for better returns of failed responses to requestor.. Prepped ex handling for future db

--- WeatherAPI/IWeatherAPI.cs
+++ WeatherAPI/IWeatherAPI.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using WeatherAPI.Models;
@@ -10,7 +11,7 @@ namespace WeatherAPI
     public interface IWeatherAPI
     {
         void StartServer();
-        Task<DailyWeatherResponse> GetDailyWeather(string lat, string lon, DateTime date);
-        Task<CurrentWeatherResponse> GetCurrentWeather(string lat, string lon, int time);
+        Task<DailyWeatherResponse> GetDailyWeather(string lat, string lon, DateTime date, HttpListenerResponse resp);
+        Task<CurrentWeatherResponse> GetCurrentWeather(string lat, string lon, int time, HttpListenerResponse resp);
     }
 }
--- WeatherAPI/WeatherAPI.cs
+++ WeatherAPI/WeatherAPI.cs
@@ -28,7 +28,7 @@ namespace WeatherAPI
             }
             catch (Exception ex)
             {
-
+                // Eventually insert into database log
             }
 
         }
@@ -78,15 +78,16 @@ namespace WeatherAPI
                         lon = req.QueryString.Get("lon");
                         time = int.Parse(req.QueryString.Get("time"));
 
-                        // Get the current weather using the lat/lon and time provided
-                        var weather = await GetCurrentWeather(lat, lon, time);
-
-                        // Alter raw data into standardized weather data, as depicted on websites
-                        weather.Data[0].Temperature = (weather.Data[0].Temperature - 273.15) * 9 / 5 + 31;  // k to f
-                        weather.Data[0].FeelsLike = (weather.Data[0].FeelsLike - 273.15) * 9 / 5 + 31;  // k to f
-                        weather.Data[0].Pressure = Math.Round(weather.Data[0].Pressure * 0.02953, 2);  // mb to inHg
-
-                        ReturnRequest(resp, weather);
+                        // Get the current weather using the lat/lon and time provided and return the response to the requestor 
+                        if (await GetCurrentWeather(lat, lon, time, resp))
+                        {
+                            // insert success into db
+                        }
+                        else
+                        {
+                            ReturnFailedRequest(resp, "Failed to make request to OpenWeatherMap");
+                            // insert failure into db
+                        }
                     }
                     else if(req.HttpMethod == "GET" && req.Url.AbsolutePath == "/api/DailyWeather/")  // DailyWeather Endpoint
                     {
@@ -95,24 +96,32 @@ namespace WeatherAPI
                         date = req.QueryString.Get("date");
 
                         // Get the current weather using the lat/lon and time provided
-                        var weather = await GetDailyWeather(lat, lon, date);
-
-                        // Alter raw data into standardized weather data, as depicted on websites
-                        weather.Temperature.Min = (weather.Temperature.Min - 273.15) * 9 / 5 + 31;  // k to f
-                        weather.Temperature.Max = (weather.Temperature.Max - 273.15) * 9 / 5 + 31;  // k to f
-
-                        ReturnRequest(resp, weather);
+                        if (await GetDailyWeather(lat, lon, date, resp))
+                        {
+                            // insert succcess into db
+                        }
+                        else
+                        {
+                            ReturnFailedRequest(resp, "Failed to make request to OpenWeatherMap");
+                            // insert failure into db
+                        }
                     }
                 }
                 catch (Exception ex) {
-
-                    byte[] buffer = Encoding.UTF8.GetBytes(ex.ToString());
-                    resp.ContentType = "text/xml";
-                    resp.ContentLength64 = buffer.Length;
-                    resp.StatusCode = (int)HttpStatusCode.OK;
-                    resp.OutputStream.Write(buffer, 0, buffer.Length);
-                }          
+                    ReturnFailedRequest(resp, ex.ToString());
+                    // Eventually insert into database log
+                }
             }
+        }
+
+        private async void ReturnFailedRequest(HttpListenerResponse response, string ex)
+        {
+            // Return error code to requester instead
+            byte[] buffer = Encoding.UTF8.GetBytes(ex.ToString());
+            response.ContentType = "text/xml";
+            response.ContentLength64 = buffer.Length;
+            response.StatusCode = (int)HttpStatusCode.OK;
+            await response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
         }
 
         private async void ReturnRequest(HttpListenerResponse response, object weather)
@@ -130,7 +139,7 @@ namespace WeatherAPI
             response.Close();
         }
 
-        public async Task<DailyWeatherResponse> GetDailyWeather(string latitude, string longitude, string date)
+        public async Task<bool> GetDailyWeather(string latitude, string longitude, string date, HttpListenerResponse resp)
         {
            
             // Get my API Key
@@ -158,22 +167,28 @@ namespace WeatherAPI
 
                         DailyWeatherResponse result = JsonConvert.DeserializeObject<DailyWeatherResponse>(responseBody);
 
-                        return result;
+                        // Alter raw data into standardized weather data, as depicted on websites
+                        result.Temperature.Min = (result.Temperature.Min - 273.15) * 9 / 5 + 31;  // k to f
+                        result.Temperature.Max = (result.Temperature.Max - 273.15) * 9 / 5 + 31;  // k to f
+
+                        ReturnRequest(resp, result);
+
+                        return true;
                     }
                     else
                     {
-                        var x = await response.Content.ReadAsStringAsync();
-                        throw new HttpRequestException(response.ReasonPhrase, new Exception(x));
+                        return false;
                     }
                 }
                 catch (Exception ex)
                 {
-                    throw ex;
+                    return false;
+                    // Eventually insert into database log
                 }
             }
         }
 
-        public async Task<CurrentWeatherResponse> GetCurrentWeather(string latitude, string longitude, int time)
+        public async Task<bool> GetCurrentWeather(string latitude, string longitude, int time, HttpListenerResponse resp)
         {
             // Get my API Key
             using (StreamReader reader = new StreamReader("C://Users/bryce/OneDrive/Documents/apiKey.txt"))
@@ -199,22 +214,27 @@ namespace WeatherAPI
                         string responseBody = await response.Content.ReadAsStringAsync();
 
                         CurrentWeatherResponse result = JsonConvert.DeserializeObject<CurrentWeatherResponse>(responseBody);
+         
+                        // Alter raw data into standardized weather data, as depicted on websites
+                        result.Data[0].Temperature = (result.Data[0].Temperature - 273.15) * 9 / 5 + 31;  // k to f
+                        result.Data[0].FeelsLike = (result.Data[0].FeelsLike - 273.15) * 9 / 5 + 31;  // k to f
+                        result.Data[0].Pressure = Math.Round(result.Data[0].Pressure * 0.02953, 2);  // mb to inHg
 
-                        return result;
+                        ReturnRequest(resp, result);
+
+                        return true;
                     }
                     else
                     {
-                        var x = await response.Content.ReadAsStringAsync();
-                        throw new HttpRequestException(response.ReasonPhrase, new Exception(x));
+                        return false;
                     }
                 }
                 catch (Exception ex)
                 {
-                    throw ex;
+                    return false;
+                    // Eventually insert into database log
                 }
-            }
-           
-           
+            }                    
         }
     }
 }
